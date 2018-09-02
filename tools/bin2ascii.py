3a0a8caa09db9842d8c79afee1b66e28ac5ddbd0
sql3t0 <dj.sqleto@gmail.com>
2018-09-02 14:33:20 -0300

    Update bin2ascii.py

--- tools/bin2ascii.py
+++ tools/bin2ascii.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+#
 # Sql3t0
 # chmod +x bin2ascii.py && mov bin2ascii.py bin2ascii && mv bin2ascii /usr/bin
 import sys
@@ -6,15 +7,33 @@ import sys
 def chunkstring(string, length):
     return (string[0+i:length+i] for i in range(0, len(string), length))
 
-if len(sys.argv) > 1:
-    text = str(sys.argv[1]).replace(' ','')
+def conv(data):
+    text = str(data).replace(' ','')
     str_ = ""
     for code in chunkstring(text,8): 
         code = int(code, 2)
         char = chr(code)
         str_ += char
-    	
-    print(str_)
+        
+    return str_
+
+
+if len(sys.argv) <= 1 :
+    if not sys.stdin.isatty():
+        data = sys.stdin.read()
+        print conv(data.replace('\n',''))
+    else:
+        print('Usage : scriptname.py "string_sequency"')
+        print('      : or ')
+        print('      : STDOUT | scriptname.py ')
 else:
-    print('Usage : bin2ascii "binary_text"')
+    if not sys.stdin.isatty():
+        data = sys.stdin.read()
+    else:
+        data = sys.argv[1]
+    
+    print conv(data.replace('\n',''))
+
+    
+
     
