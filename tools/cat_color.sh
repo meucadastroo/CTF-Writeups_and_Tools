09af1ff5e9fdfa8cbdfed58e719cac94d0e760b4
sql3t0 <dj.sqleto@gmail.com>
2018-10-29 14:12:22 -0300

    Update cat_color.sh

--- tools/cat_color.sh
+++ tools/cat_color.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 if [ -z "$1" ]
 	then
-		echo "[!] Usage: cat_color.sh FileName"
+		echo "[!] Usage: $0 FileName"
 	else
 		"C:\\PentestBox\\base\\python\\Scripts\\pygmentize.exe" -g "$1" || "pygmentize" -g "$1"
 fi
